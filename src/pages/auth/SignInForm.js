e5e451b5ccdf68f7257abcda0599c1193e67d98b
Igor Basuga <igor.basuga@gmail.com>
2021-11-09 16:27:09 +0000

    add PostCreateForm and handlers

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -17,112 +17,101 @@ import axios from "axios";
 import { useSetCurrentUser } from "../../contexts/CurrentUserContext";
 
 function SignInForm() {
-	const setCurrentUser = useSetCurrentUser();
+  const setCurrentUser = useSetCurrentUser();
 
-	const [signInData, setSignInData] = useState({
-		username: "",
-		password: "",
-	});
+  const [signInData, setSignInData] = useState({
+    username: "",
+    password: "",
+  });
 
-	const { username, password } = signInData;
+  const { username, password } = signInData;
 
-	const history = useHistory();
+  const history = useHistory();
 
-	const [errors, setErrors] = useState({});
+  const [errors, setErrors] = useState({});
 
-	const handleChange = (e) => {
-		setSignInData({ ...signInData, [e.target.name]: e.target.value });
-	};
+  const handleChange = (e) => {
+    setSignInData({ ...signInData, [e.target.name]: e.target.value });
+  };
 
-	const handleSubmit = async (e) => {
-		e.preventDefault();
-		try {
-			const { data } = await axios.post(
-				"/dj-rest-auth/login/",
-				signInData
-			);
-			setCurrentUser(data.user);
-			history.push("/");
-		} catch (err) {
-			setErrors(err.response?.data);
-		}
-	};
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post("/dj-rest-auth/login/", signInData);
+      setCurrentUser(data.user);
+      history.push("/");
+    } catch (err) {
+      setErrors(err.response?.data);
+    }
+  };
 
-	return (
-		<Row className={styles.Row}>
-			<Col className="my-auto p-0 p-md-2" md={6}>
-				<Container className={`${appStyles.Content} p-4 `}>
-					<h1 className={styles.Header}>sign in</h1>
-					<Form onSubmit={handleSubmit}>
-						<Form.Group
-							className={styles.Input}
-							controlId="username"
-						>
-							<Form.Label className="d-none">Username</Form.Label>
-							<Form.Control
-								type="text"
-								placeholder="Username"
-								name="username"
-								value={username}
-								onChange={handleChange}
-							/>
-						</Form.Group>
-						{errors.username?.map((message, idx) => (
-							<Alert variant="warning" key={idx}>
-								{message}
-							</Alert>
-						))}
+  return (
+    <Row className={styles.Row}>
+      <Col className="my-auto p-0 p-md-2" md={6}>
+        <Container className={`${appStyles.Content} p-4 `}>
+          <h1 className={styles.Header}>sign in</h1>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group className={styles.Input} controlId="username">
+              <Form.Label className="d-none">Username</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Username"
+                name="username"
+                value={username}
+                onChange={handleChange}
+              />
+            </Form.Group>
+            {errors.username?.map((message, idx) => (
+              <Alert variant="warning" key={idx}>
+                {message}
+              </Alert>
+            ))}
 
-						<Form.Group
-							className={styles.Input}
-							controlId="password"
-						>
-							<Form.Label className="d-none">Password</Form.Label>
-							<Form.Control
-								type="password"
-								placeholder="Password"
-								name="password"
-								value={password}
-								onChange={handleChange}
-							/>
-						</Form.Group>
-						{errors.password?.map((message, idx) => (
-							<Alert variant="warning" key={idx}>
-								{message}
-							</Alert>
-						))}
-						<Button
-							className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`}
-							type="submit"
-						>
-							Sign In
-						</Button>
-						{errors.non_field_errors?.map((message, idx) => (
-							<Alert variant="warning" key={idx} className="mt-3">
-								{message}
-							</Alert>
-						))}
-					</Form>
-				</Container>
-				<Container className={`mt-3 ${appStyles.Content}`}>
-					<Link className={styles.Link} to="/signup">
-						Don't have an account? <span>Sign up now!</span>
-					</Link>
-				</Container>
-			</Col>
-			<Col
-				md={6}
-				className={`my-auto d-none d-md-block p-2 ${styles.SignInCol}`}
-			>
-				<Image
-					className={`${appStyles.FillerImage}`}
-					src={
-						"https://codeinstitute.s3.amazonaws.com/AdvancedReact/hero.jpg"
-					}
-				/>
-			</Col>
-		</Row>
-	);
+            <Form.Group className={styles.Input} controlId="password">
+              <Form.Label className="d-none">Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                name="password"
+                value={password}
+                onChange={handleChange}
+              />
+            </Form.Group>
+            {errors.password?.map((message, idx) => (
+              <Alert variant="warning" key={idx}>
+                {message}
+              </Alert>
+            ))}
+            <Button
+              className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`}
+              type="submit"
+            >
+              Sign In
+            </Button>
+            {errors.non_field_errors?.map((message, idx) => (
+              <Alert variant="warning" key={idx} className="mt-3">
+                {message}
+              </Alert>
+            ))}
+          </Form>
+        </Container>
+        <Container className={`mt-3 ${appStyles.Content}`}>
+          <Link className={styles.Link} to="/signup">
+            Don't have an account? <span>Sign up now!</span>
+          </Link>
+        </Container>
+      </Col>
+      <Col
+        md={6}
+        className={`my-auto d-none d-md-block p-2 ${styles.SignInCol}`}
+      >
+        <Image
+          className={`${appStyles.FillerImage}`}
+          src={"https://codeinstitute.s3.amazonaws.com/AdvancedReact/hero.jpg"}
+        />
+      </Col>
+    </Row>
+  );
 }
 
 export default SignInForm;
