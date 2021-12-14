1634cbf211b7dbd09b7944cd90bd7e1ad406cc58
Igor Basuga <igor.basuga@gmail.com>
2021-12-14 14:53:12 +0000

    complete profile page and profile contexts challenge

---
+++ src/pages/profiles/PopularProfiles.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react";
+import appStyles from "../../App.module.css";
+import { Container } from "react-bootstrap";
+import { axiosReq } from "../../api/axiosDefaults";
+import { createBootstrapComponent } from "react-bootstrap/esm/ThemeProvider";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import Asset from "../../components/Asset";
+import Profile from "./Profile";
+
+const PopularProfiles = ({ mobile }) => {
+  const { popularProfiles } = useProfileData();
+
+  return (
+    <Container
+      className={`{appStyles.Content} ${
+        mobile && "d-lg-none text-center mb-3"
+      }`}
+    >
+      {popularProfiles.results.length ? (
+        <>
+          <p>Most followed profiles.</p>
+          {mobile ? (
+            <div className="d-flex justify-content-around">
+              {popularProfiles.results.slice(0, 4).map((profile) => (
+                <Profile key={profile.id} profile={profile} mobile />
+              ))}
+            </div>
+          ) : (
+            popularProfiles.results.map((profile) => (
+              <Profile key={profile.id} profile={profile} />
+            ))
+          )}
+        </>
+      ) : (
+        <Asset spinner />
+      )}
+    </Container>
+  );
+};
+
+export default PopularProfiles;
